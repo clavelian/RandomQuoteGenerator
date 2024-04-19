06eea5a313b30c1cc14a5051549e9d894a05f9a5
Clavelian <ianclavel1991@gmail.com>
2024-04-19 19:02:10 +0800

    feat: added Tailwindcss
    added Tailwindcss

--- prq/src/App.jsx
+++ prq/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
 
   return (
     <>
+      <h1 className='text-red-500'>Testing waters</h1>
+      <h1>Testing waters</h1>
+      <h1>Testing waters</h1>
       <h1>Testing waters</h1>
     </>
   )
