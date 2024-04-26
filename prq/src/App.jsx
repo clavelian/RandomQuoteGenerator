17e96b012d9ea6f9419f1fd6fe145277a10e1392
Clavelian <ianclavel1991@gmail.com>
2024-04-26 23:28:44 +0800

    unknown error - roadblock

--- prq/src/App.jsx
+++ prq/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import TextGenerateEffectDemo from './pages/TextGenerateEffectDemo'
-
+import helloWorld from './pages/HelloWorld'
 
 function App() {
   
   return (
     <>
       <TextGenerateEffectDemo />
-
+      <helloWorld />
     </>
   )
 }
---
+++ prq/src/pages/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const helloWorld = () => {
+  return (
+    <>
+      <h1>Hello Mother Fuckers</h1>
+    </>
+  );
+};
+
+export default helloWorld;
