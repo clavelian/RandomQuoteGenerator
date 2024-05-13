b88449f86413990a07063644effe62ab25722556
Ian Clavel <ianclavel1991@gmail.com>
2024-05-13 23:56:04 +0800

    Added

--- prq/src/services/api/AnimeGen.tsx
+++ prq/src/services/api/AnimeGen.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 function AQuote() {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
+  const [content, setAContent] = useState("");
+  const [author, setAAuthor] = useState("");
 }
 
 const AnimeGen = () => {
-  fetch("https://animechan.xyz/api/random")
-    .then((response) => response.json())
-    .then((quote) => console.log(quote));
+    axios
+    .get("https://animechan.xyz/api/random")
+    .then((response) => {
+        console.log(response.data.content, "by", response.data.author);
+        setAContent(response.data.content);
+        setAAuthor(response.data.author);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
+//   fetch("https://animechan.xyz/api/random")
+//     .then((response) => response.json())
+//     .then((quote) => console.log(quote));
 
   return <></>;
 };
